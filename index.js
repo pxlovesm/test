bba2ee16070880c6b1d8e62f3ef7abd38294ddea
pxlovesm <35925699+pxlovesm@users.noreply.github.com>
2019-05-14 17:52:54 +0800

    update3

--- index.js
+++ index.js
@@ -16,3 +16,26 @@ app.get("/hi",(request,response) =>{
 
 //this last line is required by zeit since we are stateless
 module.exports = app;
+const CONNECTION_URL = "INSERT YOUR MONGODB CONNECTION URL HERE";
+const DATABASE-NAME = "Lab10";
+
+//save a new note
+app.post("/notes",(request,response) =>{
+
+	MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true },(error,client) => {
+		if(error){
+			response.send(error);
+			throw error;
+		}
+		database = client.db(DATABASE_NAME);
+		collection = database.collection("Notes");
+
+		collection.insert(request.body, (error, result) => {
+			if(error){
+				return response.status(500).send(error);
+			}
+			response.send(result.result);
+		})
+	})
+});
+//we will use the following template for notes: '{"name":"","body":""}'
