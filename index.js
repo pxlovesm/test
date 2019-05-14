72e140f7f949bc0b9bda0c3524037e4f96951fdd
pxlovesm <35925699+pxlovesm@users.noreply.github.com>
2019-05-14 18:12:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -39,3 +39,49 @@ app.post("/notes",(request,response) =>{
 	})
 });
 //we will use the following template for notes: '{"name":"","body":""}'
+
+//get all notes
+app.get("/notes",(request, response) =>{
+
+	MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true },(error,client) =>{
+		if (error) {
+			response.send(error);
+			throw error;
+		}
+		database = client.db(DATABASE_NAME);
+		collection = database.collection("Notes");
+
+
+		collection.find({}).toArray((error,result) => {
+			if (error) {
+				return response.status(500).send(error);
+			}
+			response.send(result);
+		});
+	});
+});
+
+//get a single note
+app.get("/notes/:id",(request,response)=>{
+
+	MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true},(error,client) => {
+		if (error) {
+			response.send(error);
+			throw error;
+		}
+		database = client.db(DATABASE_NAME);
+		collection = database.collection("Notes");
+
+		collection.find({}).toArray((error,result) => {
+			if (error) {
+				return response.status(500).send(error);
+			}
+			  var numberID = parserInt(request.params.id);
+
+			  if(numberID >= result.length)
+			  	response.send("Not enough elements in database")
+			  else
+			  	response.send(result[numberID]);
+		});
+	});
+});
